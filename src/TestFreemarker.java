36dfc9e11331f5e4d5b7d4e8fda4a88cb3588a5d
wjhx <1466519819@qq.com>
2019-03-26 19:00:02 +0800

    freemark

--- src/TestFreemarker.java
+++ src/TestFreemarker.java
@@ -1,10 +1,10 @@
 
-
-
 import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Map;
+
+import com.hp.hpl.sparta.xpath.ThisNodeTest;
 
 import freemarker.cache.StringTemplateLoader;
 import freemarker.template.Configuration;
@@ -13,36 +13,61 @@ import freemarker.template.TemplateException;
 
 public class TestFreemarker {
 
-    /**
-     * @param args
-     * @throws IOException 
-     * @throws TemplateException 
-     */
-    public static void main(String[] args) throws IOException, TemplateException {
+	/**
+	 * @param args
+	 * @throws IOException
+	 * @throws TemplateException
+	 */
+	public static void main(String[] args) throws IOException, TemplateException {
 
-        
-        Configuration cfg = new Configuration();
-        StringTemplateLoader StringTemplateLoader1 = new StringTemplateLoader();
-        String templateContent="欢迎：${name} 我在${地点}";
-        StringTemplateLoader1.putTemplate("myTemplate",templateContent);
-        
-        cfg.setTemplateLoader(StringTemplateLoader1);
-        
-        
-            Template template = cfg.getTemplate("myTemplate","utf-8");
-            Map root = new HashMap();  
-            root.put("name", "javaboy2012");  root.put("地点", "cantonxxxxxxx");
-            
-            StringWriter writer = new StringWriter();  
-           
-                template.process(root, writer);
-                System.out.println(writer.toString());  
-           
+		StringWriter stringWriter = new StringWriter() {
+			{
+				new Configuration() {
+					{
+						setTemplateLoader(new StringTemplateLoader() {
+							{
+								putTemplate("myTemplate", "欢迎：${name} 我在${地点}");// name, templateSource);
+							}
+						});
+					}
+				}.getTemplate("myTemplate", "utf-8").process(new HashMap() {
+					{
+						put("name", "javaboy2012");
+						put("地点", "cantonxxxxxxx");
 
+					}
+				}, this);
 
+			}
+		};
+		System.out.println(stringWriter.toString());
+		
+		System.out.println( new HashMap() {{
+			put("key", "aaaa");
+		}}.get("key") );
 
+	}
 
+	private static void t2() throws IOException, TemplateException {
+		Configuration cfg = new Configuration() {
+			{
+				this.setTemplateLoader(new StringTemplateLoader() {
+					{
+						this.putTemplate("myTemplate", "欢迎：${name} 我在${地点}");// name, templateSource);
+					}
+				});
+			}
+		};
 
-    }
+		Template template = cfg.getTemplate("myTemplate", "utf-8");
+		Map root = new HashMap();
+		root.put("name", "javaboy2012");
+		root.put("地点", "cantonxxxxxxx");
+
+		StringWriter writer = new StringWriter();
+
+		template.process(root, writer);
+		System.out.println(writer.toString());
+	}
 
 }
